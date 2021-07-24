cffe41d7663054d2612d6847f072f672568c3be7
chrzyki <christoph@functor-argument.net>
2021-07-24 06:02:25 +0200

    Release prep

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # CLDF dataset derived from Mann's "Phonological Reconstruction of Proto Northern Burmic" from 1998
 
-Cite the source dataset as
+[![CLDF validation](https://github.com/lexibank/mannburmish/workflows/CLDF-validation/badge.svg)](https://github.com/lexibank/mannburmish/actions?query=workflow%3ACLDF-validation)
 
-> Mann, Noel W. 1998. A phonological reconstruction of Proto Northern Burmic. (PhD Thesis).
+## How to cite
+
+If you use these data please cite
+- the original source
+  > Mann, Noel W. 1998. A phonological reconstruction of Proto Northern Burmic. (PhD Thesis).
+- the derived dataset using the DOI of the [particular released version](../../releases/) you were using
+
+## Description
+
 
 This dataset is licensed under a CC-BY-4.0 license
 
@@ -20,9 +28,9 @@ This dataset was originally digitized by Nathan W. Hill, and then later converte
 ## Statistics
 
 
-[![Build Status](https://travis-ci.org/lexibank/mannburmish.svg?branch=master)](https://travis-ci.org/lexibank/mannburmish)
+[![CLDF validation](https://github.com/lexibank/mannburmish/workflows/CLDF-validation/badge.svg)](https://github.com/lexibank/mannburmish/actions?query=workflow%3ACLDF-validation)
 ![Glottolog: 100%](https://img.shields.io/badge/Glottolog-100%25-brightgreen.svg "Glottolog: 100%")
-![Concepticon: 99%](https://img.shields.io/badge/Concepticon-99%25-brightgreen.svg "Concepticon: 99%")
+![Concepticon: 100%](https://img.shields.io/badge/Concepticon-100%25-brightgreen.svg "Concepticon: 100%")
 ![Source: 100%](https://img.shields.io/badge/Source-100%25-brightgreen.svg "Source: 100%")
 ![BIPA: 100%](https://img.shields.io/badge/BIPA-100%25-brightgreen.svg "BIPA: 100%")
 ![CLTS SoundClass: 100%](https://img.shields.io/badge/CLTS%20SoundClass-100%25-brightgreen.svg "CLTS SoundClass: 100%")
@@ -48,3 +56,10 @@ Nathan W. Hill | @nw36 | digitization | Other
 Noel Walter Mann | | data collector | DataCollector, Author
 
 
+
+
+## CLDF Datasets
+
+The following CLDF datasets are available in [cldf](cldf):
+
+- CLDF [Wordlist](https://github.com/cldf/cldf/tree/master/modules/Wordlist) at [cldf/cldf-metadata.json](cldf/cldf-metadata.json)
--- lexibank_mannburmish.py
+++ lexibank_mannburmish.py
@@ -1,34 +1,30 @@
-import attr
-from pathlib import Path
-from csvw import Datatype
-
-from pylexibank import Concept, Language, Cognate
-from pylexibank.dataset import Dataset as BaseDataset
-from pylexibank.util import progressbar
-
-from clldutils.misc import slug
 from collections import defaultdict
+from pathlib import Path
+
+import attr
+import pylexibank
+from clldutils.misc import slug
 
 
 @attr.s
-class CustomConcept(Concept):
+class CustomConcept(pylexibank.Concept):
     Number = attr.ib(default=None)
 
 
 @attr.s
-class CustomCognate(Cognate):
+class CustomCognate(pylexibank.Cognate):
     Morpheme_Index = attr.ib(default=None)
 
 
 @attr.s
-class CustomLanguage(Language):
+class CustomLanguage(pylexibank.Language):
     Latitude = attr.ib(default=None)
     Longitude = attr.ib(default=None)
     SubGroup = attr.ib(default="Burmish")
     Family = attr.ib(default="Sino-Tibetan")
 
 
-class Dataset(BaseDataset):
+class Dataset(pylexibank.Dataset):
     dir = Path(__file__).parent
     id = "mannburmish"
     language_class = CustomLanguage
@@ -53,38 +49,31 @@ class Dataset(BaseDataset):
         languages = {}
         for language in self.languages:
             args.writer.add_language(**language)
-            languages[language['Name']] = language['ID']
+            languages[language["Name"]] = language["ID"]
 
-        data = self.raw_dir.read_csv(
-                'Mann-redo.csv',
-                delimiter='\t',
-                dicts=True)
+        data = self.raw_dir.read_csv("Mann-redo.csv", delimiter="\t", dicts=True)
 
         words = defaultdict(list)
         for i, row in enumerate(data):
             for language, lid in languages.items():
                 if row[language].strip():
-                    number = row['Mann_number'][:-1]
-                    words[lid, number] += [(row[language], row['Mann_number'],
-                        str(i+1))]
+                    number = row["Mann_number"][:-1]
+                    words[lid, number] += [(row[language], row["Mann_number"], str(i + 1))]
 
         for (language, number), values in words.items():
-            value = ' '.join([x[0] for x in values])
+            value = " ".join([x[0] for x in values])
             cogids = [x[1] for x in values]
-            locids = '-'.join([x[2] for x in values])
+            locids = "-".join([x[2] for x in values])
 
             lexeme = args.writer.add_form(
-                    Language_ID=language,
-                    Parameter_ID=concepts[str(int(number))],
-                    Local_ID='{0}-{1}'.format(language, locids),
-                    Value=value,
-                    Form=self.lexemes.get(value, value),
-                    Source=['Mann1998']
-                    )
+                Language_ID=language,
+                Parameter_ID=concepts[str(int(number))],
+                Local_ID="{0}-{1}".format(language, locids),
+                Value=value,
+                Form=self.lexemes.get(value, value),
+                Source=["Mann1998"],
+            )
             for i, cogid in enumerate(cogids):
                 args.writer.add_cognate(
-                        lexeme=lexeme,
-                        Cognateset_ID=cogid,
-                        Morpheme_Index=i+1,
-                        Source='Mann1998')
-
+                    lexeme=lexeme, Cognateset_ID=cogid, Morpheme_Index=i + 1, Source="Mann1998"
+                )
